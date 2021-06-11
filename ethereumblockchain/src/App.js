6021454f47ef1b0d88c93ce151c3820f2ba3358a
DaiTien <nguyendaimaitien@gmail.com>
2021-06-11 13:44:56 +0700

    tien new source code

---
+++ ethereumblockchain/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Web/Header';
+import HomeMain from './Components/Web/HomeMain';
+import MenuFooter from './Components/Web/MenuFooter';
+import IndexFooter from './Components/Web/Footer';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HomeMain />
+      <MenuFooter/>
+      <IndexFooter/>
+    </>
+  );
+}
+
+export default App;
